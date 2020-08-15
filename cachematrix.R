e081420748984fb5117448f1d69b222d213c9e88
Fgazzelloni <61802414+Fgazzelloni@users.noreply.github.com>
2020-08-15 16:53:57 +0200

    Update cachematrix.R
    This is the answer for assignment 2 (week3) of the R programming course. Listed are the two functions: makeCacheMatrix, cacheSolve.
    The first function creates a vector which is a list of functions to set the inverse of a special matrix object, while the second function is a function that calculates the inverse of a matrix set by the makeCacheMatrix function above.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,41 @@
 ## functions do
 
 ## Write a short comment describing this function
+##makeCacheMatrix is a vector which is a list of functions to set the inverse of a special matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y #<<- operator which can be used to assign a value to an object in 
+    #an environment that is different from the current environment.
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setinversa <- function(inverse) m <<- inverse
+  getinversa <- function() m
+  
+  list(set = set, get = get,
+       setinversa = setinversa,
+       getinversa = getinversa)
 }
 
 
 ## Write a short comment describing this function
+##cacheSolve is a function that calculates the inverse of a matrix set by the makeCacheMatrix function above 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  
+    m <- x$getinversa()
+  
+  if(!is.null(m)) {
+    message("getting cached matrix")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinversa(m)
+  m
+  }
